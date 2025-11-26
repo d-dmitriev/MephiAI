a8342ba8d0ddd9eb753ed9a5f09b45c2a9d614d1
Dmitry Dmitriev <d.dmitrieff@gmail.com>
2025-11-26 13:17:25 +0300

    Test2

--- src/main/java/home/work/SentimentApplication.java
+++ src/main/java/home/work/SentimentApplication.java
@@ -1,11 +1,18 @@
 package home.work;
 
+//import io.github.mweirauch.micrometer.jvm.extras.ProcessMemoryMetrics;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SentimentApplication {
     public static void main(String[] args) {
         SpringApplication.run(SentimentApplication.class, args);
     }
+//
+//    @Bean
+//    public ProcessMemoryMetrics processMemoryMetrics() {
+//        return new ProcessMemoryMetrics();
+//    }
 }
